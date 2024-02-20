f0d475bd2b9bb3fcfa636b6633f9c969018a9637
“Run8unny” <zaneta.wardaszka@gmail.com>
2024-02-20 15:02:33 +0100

    push projektu

---
+++ js/script.js
@@ -0,0 +1,10 @@
+function menuBurger() {
+    const btn = document.querySelector(".burger__menu");
+    const body = document.querySelector("body");
+
+    btn.onclick = () => {
+        body.classList.toggle("show-menu");
+    }
+}
+
+menuBurger();
