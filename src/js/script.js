ebdcf8981154596e24cde9fde83fa88a35e80b05
“Run8unny” <zaneta.wardaszka@gmail.com>
2024-04-03 23:11:34 +0200

    script dom changes

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,13 @@
 const formCalc = document.querySelector('.calc__form');
 const select = document.querySelector('.select__dropdown');
+const checkbox = document.querySelectorAll('.form__checkbox');
+const summary = document.querySelector('.calc__summary');
+
+class Calc {
+    constructor (form, summary) {
+
+    }
+}
 
 function menuBurger() {
     const btn = document.querySelector(".burger__menu");
